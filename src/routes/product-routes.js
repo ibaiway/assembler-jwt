4b6d67559a4426227e3163cc26ddd11dc8902e70
ibaialberdi <ibaialberdi@gmail.com>
2024-04-01 02:15:36 +0200

    Created AuthMiddleware

--- src/routes/product-routes.js
+++ src/routes/product-routes.js
@@ -6,16 +6,17 @@ import {
 	getProductById,
 	updateProductById,
 } from '../controllers/product-controller.js'
+import { verifyJWT } from '../middlewares/auth-middleware.js'
 const productRouter = Router()
 
 productRouter.get('/', getAllProducts)
 
 productRouter.get('/:id', getProductById)
 
-productRouter.post('/', createProduct)
+productRouter.post('/', verifyJWT, createProduct)
 
-productRouter.put('/:id', updateProductById)
+productRouter.put('/:id', verifyJWT, updateProductById)
 
-productRouter.delete('/:id', deleteProductById)
+productRouter.delete('/:id', verifyJWT, deleteProductById)
 
 export default productRouter
