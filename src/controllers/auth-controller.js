f475c74e1ae9dd28964da8fb473775fc8e2fcd0a
ibaialberdi <ibaialberdi@gmail.com>
2024-04-01 02:15:25 +0200

    Create accesToken and refreshToken on login

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,5 +1,6 @@
 import { User } from '../models/user-model.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export const handleRegister = async (req, res) => {
 	const { username, password } = req.body
@@ -8,7 +9,6 @@ export const handleRegister = async (req, res) => {
 			.status(400)
 			.json({ message: 'Username and password are required.' })
 
-	//check for duplicate username
 	try {
 		const isDuplicate = await User.findOne({ username })
 		if (isDuplicate) return res.sendStatus(409)
@@ -36,8 +36,28 @@ export const handleLogin = async (req, res) => {
 
 	const match = await bcrypt.compare(password, foundUser.password)
 
+	const accessToken = jwt.sign(
+		{ userId: foundUser._id, username: foundUser.username },
+		process.env.ACCESS_TOKEN_SECRET,
+		{ expiresIn: '30s' },
+	)
+
+	const refreshToken = jwt.sign(
+		{ userId: foundUser._id },
+		process.env.REFRESH_TOKEN_SECRET,
+		{ expiresIn: '1m' },
+	)
+	foundUser.refreshToken = refreshToken
+	await foundUser.save()
+
+	res.cookie('refreshToken', refreshToken, {
+		httpOnly: true,
+		secure: true,
+		maxAge: 70000,
+	})
+
 	if (match) {
-		res.status(200).json({ success: 'Logged in!' })
+		res.status(200).json({ accessToken })
 	} else {
 		res.sendStatus(401)
 	}
